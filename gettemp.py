2ccc6ee50a170b84d60a64b893d9ca3f531bf6aa
Braelan Creswell <braelan.creswell@maine.edu>
2022-04-11 19:39:14 -0400

    Changed python script to use smbus for i2c

--- gettemp.py
+++ gettemp.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
 import os
 import sys
+import smbus
 import mysql.connector
 from datetime import date, datetime
+
+bus = smbus.SMBus(1) #i2c bus 1
+i2caddr = 0x4d #i2c address for sensor
 
 # Calculate 2's complement
 def twos_comp(val, bits):
@@ -10,15 +14,11 @@ def twos_comp(val, bits):
         val = val - (1 << bits)
     return val
 
-cmd = "/usr/sbin/i2cget -y 1 0x4d 0 b" #command for getting data from sensor
-
-x = os.popen(cmd) #run command and save output to x
-
-unsignedtemp = int(x.read(), 16) #change popen data to readable hex value
+unsignedtemp = bus.read_byte(i2caddr) #read data from sensor
 
 temp = twos_comp(unsignedtemp, 8) #Take twos complement of value to account for negative temperatures
 
-temp = round(temp, 2)
+temp = round(temp, 2) #round to 2 decimal place
 
 try:
     cpufile = open("/sys/class/thermal/thermal_zone0/temp", "r") #open core temp file
@@ -30,16 +30,16 @@ cpu = int(cpufile.read()) #read core temp
 cpu = round(cpu/1000, 2) #make core temp readible
 
 try:
-    data = mysql.connector.connect(user='datalog', password='diViqIgan1G3', host='127.0.0.1', database='datalogger')
+    data = mysql.connector.connect(user='datalog', password='diViqIgan1G3', host='127.0.0.1', database='datalogger') #open connection to MySQL
     send = data.cursor()
-    timestamp = datetime.now()
-    query = ("INSERT INTO temperature (cputemp, roomtemp, timestamp) VALUES (%s, %s, %s)")
-    querydata = (cpu, temp, timestamp)
-    send.execute(query, querydata)
-    data.commit()
+    timestamp = datetime.now() #take timestamp
+    query = ("INSERT INTO temperature (cputemp, roomtemp, timestamp) VALUES (%s, %s, %s)") #build sql query
+    querydata = (cpu, temp, timestamp) #variables to insert into sql
+    send.execute(query, querydata) #Run query
+    data.commit() #ensure data is saved in database
 except:
-    print("MySQL Error")
+    print("MySQL Error") #handle failure
     quit()
 
-data.close()
+data.close() #disconnect from MySQL
 
